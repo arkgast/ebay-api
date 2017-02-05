f2ec03f08b479525f555528ae61831bfd2850b00
Arnold Gandarillas Castillo <a.kltrx@gmail.com>
2017-02-04 23:58:52 -0400

    Add database methods

---
+++ core/database.php
@@ -0,0 +1,60 @@
+<?php
+
+abstract class Database {
+  private $HOST = 'localhost';
+  private $USER = 'root';
+  private $PASSWORD = '';
+  private $DATABASE = 'ebay';
+  protected $conn;
+  protected $query;
+
+  public function __construct() {
+    $this->conn = mysqli_connect($this->HOST, $this->USER, $this->PASSWORD, $this->DATABASE);
+    if (!$this->conn)
+      echo "Can't connect";
+  }
+
+  public function fetch_data() {
+    $result = $this->conn->query($this->query);
+    $rows = $result->fetch_all(MYSQLI_ASSOC);
+    $result->free();
+    return $rows;
+  }
+
+  public function execute_query(...$params) {
+    $stmt = $this->conn->prepare($this->query);
+    $sLen = str_repeat('s', count($params));
+    $stmt->bind_param($sLen, ...$params);
+    $stmt->execute();
+    $stmt->close();
+  }
+  
+}
+
+
+class EbaySearchLog extends Database {
+
+  public function insertRow(...$params) {
+    $this->serializeField(...$params);
+    $this->query = 'insert into search_log(title, seller, price, images) values(?, ?, ?, ?)';
+    $this->execute_query(...$params);
+  }
+
+  public function deleteRow($id) {
+    $this->query = 'delete from search_log where id = ?';
+    $this->execute_query($id);
+  }
+
+  public function list() {
+    $this->query = 'select id, title, seller, price, images from search_log';
+    return $this->fetch_data();
+  }
+
+  /**
+   * Serialize arr img field
+   */
+  protected function serializeField(&...$arr) {
+    $arr[3] = serialize($arr[3]);
+  }
+
+}
--- search.php
+++ search.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'core/search_item.php';
+require_once 'core/database.php';
 
 $itemId = $_POST['item-id'];
 
@@ -14,3 +15,11 @@ echo "Price: {$item->ConvertedCurrentPrice} <br/>";
 foreach ($images as $image) {
   echo "<img src={$image} width=150 height=180>";
 }
+
+echo "<br><=============================><br>";
+
+$db = new EbaySearchLog();
+$list = $db->list();
+print_r($list);
+$db->insertRow('title', 'seller1', 8.45, 'img1');
+$db->deleteRow(7);
