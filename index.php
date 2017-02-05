617adb03352bd8d71c14a9cf1109da580d0d217d
Arnold Gandarillas Castillo <a.kltrx@gmail.com>
2017-02-04 21:17:11 -0400

    Init app

---
+++ index.php
@@ -0,0 +1,12 @@
+<!Doctype html>
+<html>
+  <head>
+    <title>Ebay - Search Item</title>
+  </head>
+  <body>
+    <form action="./search.php" method='post'>
+      <input type="text" name="item-id">
+      <button type='submit'>Search</button>
+    </form>
+  </body>
+</html>
